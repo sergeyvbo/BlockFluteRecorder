6eb76c11636de8c01fa3e0a342e8527cc393d076
Sergey B <sergeyvbo@outlook.com>
2021-03-14 23:09:07 +0300

    редактор трека и палитра нот перенесены в компоненты

--- BlockFluteRecorder/Shared/Canvas.razor.cs
+++ BlockFluteRecorder/Shared/Canvas.razor.cs
@@ -1,25 +1,41 @@
 ﻿using BlockFluteRecorder.Model;
+using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace BlockFluteRecorder.Shared
 {
     public partial class Canvas
     {
+        public IJSRuntime js;
         public string Title { get; set; }
         public bool IsPlaying { get; set; } = false;
-        public List<Note> Track { get; set; }
+        public List<Note> Track { get; set; } = new List<Note>();
         public void PlayPause() => IsPlaying = !IsPlaying;
         public void Stop()
         {
             IsPlaying = false;
         }
 
+        public void AddNote(Note note)
+        {
+            Stop();
+            Track.Add(note);
+            StateHasChanged();
+        }
+
         public void DeleteNote(Note note)
         {
+            Stop();
             Track.Remove(note);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("printing");
         }
     }
 }
