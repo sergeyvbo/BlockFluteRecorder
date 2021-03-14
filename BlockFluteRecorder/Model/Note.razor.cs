522b3a961800f0a4f211e03a45205c2d887b095a
Sergey B <sergeyvbo@outlook.com>
2021-03-14 22:23:39 +0300

    Добавьте файлы проекта.

---
+++ BlockFluteRecorder/Model/Note.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockFluteRecorder.Model
+{
+    public partial class Note
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ BlockFluteRecorder/Shared/Canvas.razor.cs
@@ -0,0 +1,25 @@
+﻿using BlockFluteRecorder.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockFluteRecorder.Shared
+{
+    public partial class Canvas
+    {
+        public string Title { get; set; }
+        public bool IsPlaying { get; set; } = false;
+        public List<Note> Track { get; set; }
+        public void PlayPause() => IsPlaying = !IsPlaying;
+        public void Stop()
+        {
+            IsPlaying = false;
+        }
+
+        public void DeleteNote(Note note)
+        {
+            Track.Remove(note);
+        }
+    }
+}
